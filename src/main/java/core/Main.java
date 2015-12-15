ee698dd4d437ef160ab1c020b4b80580559497c7
MasterMoritz <andreas.moritz@student.uibk.ac.at>
2015-12-15 12:28:07 +0100

    remove endpoint examples

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -86,51 +86,6 @@ public class Main {
 			return sj.toString();
 		});
 
-        /* Example for the 5gig API */
-        get("/5gigtest", (req, res) -> {
-        	try {
-        		/* Jackson ObjectMapper maps JSON to an arbitrary Java class. */
-	        	ObjectMapper mapper = new ObjectMapper();
-	        	// Got a Java class that data maps to nicely? If so:
-	        	//		MyFancyClass mfc = mapper.readValue(url, MyFancyClass.class);
-	        	//
-	        	// Or: if no class (and don't need one), just map to Map.class:
-	        	//		Map<String,Object> map = mapper.readValue(url, new TypeReference<Map<String, Object>>(){});
-	        	
-	        	URL endpoint = new URL("http://www.nvivo.es/api/request.php?api_key=90c164a1d82540d7be50d54f4e887cb2&method=user.getEvents&user=hermzz&format=json");
-	        	Map<String,Object> map = mapper.readValue(endpoint, new TypeReference<Map<String, Object>>(){}); 	//TypeReference is only needed to pass generic type definition
-	        	/* Display part of the parsed JSON */
-	        	return map.get("status");
-        	}
-        	catch (JsonParseException e) {
-	            res.status(400);
-	            return "";
-        	}
-        });
-        
-        /* Example for using the kimono endpoint URL (webhook is better) */
-        get("/kimono_no_webhook", (req, res) -> {
-        	try {
-	        	/* Jackson ObjectMapper maps JSON to an arbitrary Java class. */
-	        	ObjectMapper mapper = new ObjectMapper();
-	        	URL endpoint = new URL("https://www.kimonolabs.com/api/1x4z55uw?apikey=cZIBnjuI5UUN5K420SiYnxeEci5GjKHs");
-	        	WebhookData newData = mapper.readValue(endpoint, WebhookData.class);
-	        	for(EventInfo elem : newData.getResults().getEventInfos()) {
-		        	if (!elem.validate()) {
-		                res.status(400);
-		                return "";
-		            }
-	        	}
-	        	for(EventInfo elem : newData.getResults().getEventInfos()) {
-	        		System.out.println(elem.getPrice());
-	        	}
-	        	return "OK";
-        	}
-        	catch (JsonParseException e) {
-	            res.status(400);
-	            return "";
-        	}
-        });
         
         /* Example webhook integrating with kimono */
         post("/webhook", (req, res) -> {	//we specify 'ibkscraper.herokuapp.com/webhook' as the url to post to (in kimono)
