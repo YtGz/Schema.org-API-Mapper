0b2e87696059bfb3d32c5dc84d54e4d12bd01877
YtGz <Philipp.Wirtenberger@student.uibk.ac.at>
2018-01-24 19:20:52 +0100

    add check for content type

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
 	//To access the application, browse to http://localhost:4567
     public static void main(String[] args) {
-    	get("/", (req, res) -> "Hello World");
+    	before((req, res) -> {
+    		if(!req.contentType().equalsIgnoreCase("application/json")) {
+    			halt("Content type does not match \"application/json\"!");
+    		}
+    	});
+    	
+    	post("/events/categories/add", (req, res) -> {
+    		//TODO: parse the JSON from the request
+    		
+    		//TODO: call the eventful API endpoint
+    		
+    		//TODO: modify the response of the eventful API call
+    		
+    		
+    		res.type("application/json"); 
+    		return req.body();
+    	});
     }
 }
